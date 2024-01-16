58213478ed84f8cf8f5d22d72906ebf904e2bf3f
owner <oredev@proton.me>
2024-01-16 16:53:43 +0545

    The dockerfile has been modifed such that local development is possible due to volumes

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Local Hot Reload able dockerized environment is ready! </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
